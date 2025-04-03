792ee219a8d55720e7c6aab9921a3b970e7346f9
Vitor Zucher <vzucher@gmail.com>
2025-04-03 09:12:58 -0300

    fix: increase ISR page size limit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  experimental: {
+    largePageDataBytes: 20 * 1024 * 1024, // 20MB
+  },
 };
 
 module.exports = nextConfig; 
